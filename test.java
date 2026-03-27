13f1dafa79d0e92130f9475e4b14252c49ecdb87
Truty424 <86971161+Truty424@users.noreply.github.com>
2026-03-27 10:45:58 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,24 +1,25 @@
-import org.bouncycastle.crypto.engines.AESEngine;
-import org.bouncycastle.crypto.engines.ChaChaEngine;
-import org.bouncycastle.crypto.modes.CBCBlockCipher;
-import org.bouncycastle.crypto.paddings.PKCS7Padding;
+import java.util.Arrays;
+import org.bouncycastle.crypto.Wrapper;
+import org.bouncycastle.crypto.engines.AESWrapEngine;
 import org.bouncycastle.crypto.params.KeyParameter;
-import org.bouncycastle.crypto.params.ParametersWithIV;
 
-public class BcModeTestFile {
+public class BcWrapUnwrapExample {
+    public static void main(String[] args) throws Exception {
+        byte[] kek = new byte[16];
+        byte[] keyToWrap = new byte[16];
 
-    public void encryptAESCBCPKCS7(byte[] key, byte[] iv) {
-        AESEngine aes = AESEngine.newInstance(); // Noncompliant {{AES}}
-        CBCBlockCipher cbc = CBCBlockCipher.newInstance(aes); // Noncompliant {{AES-CBC}}
-        PKCS7Padding padding = new PKCS7Padding(); // Noncompliant {{PKCS7}}
+        Arrays.fill(kek, (byte) 0x11);
+        Arrays.fill(keyToWrap, (byte) 0x22);
 
-        cbc.init(true, new ParametersWithIV(new KeyParameter(key), iv));
-        return;
-    }
+        Wrapper wrapper = new AESWrapEngine();
 
-    public void decryptChaCha20(byte[] key, byte[] iv) {
-        ChaChaEngine chaCha20 = new ChaChaEngine(20); // Noncompliant {{ChaCha20}}
-        chaCha20.init(false, new ParametersWithIV(new KeyParameter(key), iv));
-        return;
+        wrapper.init(true, new KeyParameter(kek));
+        byte[] wrapped = wrapper.wrap(keyToWrap, 0, keyToWrap.length);
+
+        wrapper.init(false, new KeyParameter(kek));
+        byte[] unwrapped = wrapper.unwrap(wrapped, 0, wrapped.length);
+
+        System.out.println("wrapped len = " + wrapped.length);
+        System.out.println("unwrapped len = " + unwrapped.length);
     }
 }
